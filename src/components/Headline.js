f51e61bc9897bbad7690e0db9d5bd9f2650c4bdd
IvanGent <gentilleivan@gmail.com>
2021-03-31 13:54:57 -0400

    styling for headline added

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 function Headline({head}) {
-    return <div>{head}</div>
+
+    const style = {
+        width: '38.25rem',
+        height: '17rem',
+        margin: '10.125rem 10.688rem 2.938rem 0',
+        fontFamily: 'HelveticaNeue',
+        fontSize: '5.25rem',
+        fontWeight: 'bold',
+        fontStretch: 'normal',
+        fontStyle: 'normal',
+        lineHeight: '1',
+        letterSpacing: '-2.2px',
+        color: 'white',
+    }
+
+    return <div style={style}>{head}</div>
 }
 
 export default Headline;
