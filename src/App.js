b6dc40a348a3e81f8d737d744805d2721067398c
IvanGent <gentilleivan@gmail.com>
2021-03-31 10:55:47 -0400

    adding navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 // import './App.css';
+import NavBar from './components/navBar';
 
 function App() {
   return (
     <div className="App">
-        Hello, Is this working???
+      <NavBar />
+      Hello, Is this working???
     </div>
   );
 }
