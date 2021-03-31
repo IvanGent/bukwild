c5d3ab6c2485641f4145a5c6038aeeac2ee79e69
IvanGent <gentilleivan@gmail.com>
2021-03-31 13:42:28 -0400

    styling Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,15 +6,33 @@ const style = {
     height: '4.875rem',
     margin: '0.313rem 59.875rem 0 0',
     fontFamily: 'HelveticaNeue',
-    fontSize: '0.875rem'
+    fontSize: '0.875rem',
+    fontWeight: 'bold',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    lineHeight: '1.86',
+    letterSpacing: 'normal',
+    color: 'white',
+    backgroundColor: 'gray',
+    display: 'flex',
+    flexWrap: 'wrap'
+}
+
+const active = {
+    color: '#ffc004'
+}
+
+const links = {
+    color: 'white',
+    textDecoration: 'none',
 }
 
 function Menu () {
     return (
         <div style={style}>
-            <NavLink to='/industries' >Industries</NavLink>
-            <NavLink to='/services' >Services</NavLink>
-            <NavLink to='/aboutUs' >About Us</NavLink>
+            <NavLink to='/industries' style={links} activeStyle={active}>Industries</NavLink>
+            <NavLink to='/services' style={links} activeStyle={active}>Services</NavLink>
+            <NavLink to='/aboutUs' style={links} activeStyle={active}>About Us</NavLink>
         </div>
     )
 }
