d6ecc1a9da60857b3d586e3d23ecd511912b2da3
IvanGent <gentilleivan@gmail.com>
2021-03-31 13:20:37 -0400

    adding photos and displaying background images

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Headline from './Headline';
 import SubHead from './SubHead';
-import CallToAction from './CallToAction';;
+import CallToAction from './CallToAction';
 
 function Marquee({info}) {
+
+    useEffect(() => {
+        console.log(info.background)
+    })
+
+    const style = {
+        backgroundImage: `url(/images/${info.background})`,
+        color: 'red',
+    }
+
     return (
-        <div>
+        <div style={style}>
             <Headline head={info.headline} />
             <SubHead sub={info.subhead} />
             <CallToAction cta={info.cta} />
